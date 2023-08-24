8b5ec2d463c869be64a02ac042b4d63c0d529145
ahmedjp143 <ajb1434@gmail.com>
2023-08-24 19:15:18 +0100

    express generate

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-console.log('ahmed');
-console.log('hassan');
-console.log('hi boss');
+const express = require('express');
+const app = express();
+app.use(express.json());
+const port = 3001;
+app.get('/', function (req, res) {
+  res.send('welcome to resturent backend');
+});
+app.listen(port);
+console.log(port);
