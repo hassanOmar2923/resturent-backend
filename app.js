faee94861ea4c2df0d26026054bd609ae11f6864
ahmedjp143 <ajb1434@gmail.com>
2023-08-24 19:24:40 +0100

    add express funtion

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const port = 3001;
+
 app.get('/', function (req, res) {
   res.send('welcome to resturent backen');
 });
+
 app.listen(port);
 console.log(port);
