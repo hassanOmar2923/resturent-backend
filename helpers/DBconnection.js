1591bbad02265ca9c4fdfd8e30913a8f1ab0f164
hassandev <134854047+hassanOmar2923@users.noreply.github.com>
2024-07-22 11:01:10 +0300

    Update DBconnection.js

--- helpers/DBconnection.js
+++ helpers/DBconnection.js
@@ -6,7 +6,7 @@ mongoose.set('strictQuery', false);
 //   return (
 //     mongoose
 //       .connect('mongodb://127.0.0.1:27017/resturent')
-//       // .connect('mongodb+srv://xasancumar:1zhHPp6abdOhvt1k@cluster0.d4o1wlf.mongodb.net/restaurent')
+
 //       .then(() => console.log('Connected DATABASE!'))
 //       .catch(() => console.log(' connection failed'))
 //   );
@@ -14,7 +14,7 @@ mongoose.set('strictQuery', false);
 function mongoDB(){
     return mongoose
     .connect('mongodb://127.0.0.1:27017/restaurent')
-    // .connect('mongodb+srv://xasancumar:1zhHPp6abdOhvt1k@cluster0.d4o1wlf.mongodb.net/restaurent')
+  
     .then(() => console.log('Connected DATABASE!'))
     .catch(() => console.log(' connection failed'));
   
